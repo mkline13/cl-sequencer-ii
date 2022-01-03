9b4238648338e078e6123310ff02fdf3a83976e4
Mason Kline <mkline13@gmail.com>
2022-01-02 23:35:10 -0800

    cleaned up unused code

--- app.py
+++ app.py
@@ -3,24 +3,17 @@ from mediator import Mediator
 from player import Player
 from transport import Transport
 
-from threading import Thread
-
 
 class App:
     def __init__(self):
         # instantiate all application parts
         self.transport = Transport()
         self.player = Player(self.transport)
-        self.parser = None
-        self.output = None
         self.mediator = Mediator(self)
         self.cli = CLI(self.mediator)
 
         # make any necessary connections between them
         self.playback_thread = None
-
-    def create_playback_thread(self):
-        self.playback_thread = Thread(target=self.player.run)
 
     def run(self):
         # launch cli repl
